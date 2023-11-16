d14e48793f74f0c1ed4742503c2afd16dfe5cb9d
KathleenSantos <kathleen.cristine@incentive.me>
2023-11-16 16:06:03 -0300

    Style: create base to global style for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 
+import { GlobalStyle } from './styles/global.ts'
+
 export function App() {
 
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1>Hello world</h1>
+      <GlobalStyle />
+    </>
   )
 }
