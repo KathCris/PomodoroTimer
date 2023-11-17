ff840b6d19ae4c72a876cb6e2b56f907acb249c4
KathleenSantos <kathleen.cristine@incentive.me>
2023-11-17 14:52:16 -0300

    Feature: Create routers in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { ThemeProvider } from 'styled-components'
 
+import { Router } from './Router.tsx'
+import { BrowserRouter } from 'react-router-dom'
+
 import { GlobalStyle } from './styles/global.ts'
 import { defaultTheme } from './styles/themes/default.ts'
 
@@ -7,6 +10,9 @@ export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <h1>Hello world</h1>
       <GlobalStyle />
     </ThemeProvider>
