67f0d2bd595d4bdb524a60ce8a8a18bfc6dd3155
KathleenSantos <kathleen.cristine@incentive.me>
2023-12-15 12:11:41 -0300

    Feat: create countDown to timer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,8 @@ import { CountdowmContainer, FormContainer, HomeContainer, MinuteAmountInput, Se
 import { useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as zod from 'zod';
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from 'date-fns'
 
 
 const newCycleFormValidationSchema = zod.object({
@@ -12,7 +14,18 @@ const newCycleFormValidationSchema = zod.object({
 
 type neyCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date 
+}
+
 export function Home() {
+
+  const [cycles, setCycles] = useState<Cycle[]>([]) 
+  const [activeCycle, setActiveCycle] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
   const { register, handleSubmit, watch, reset } = useForm({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -23,12 +36,45 @@ export function Home() {
   })
 
   function handleCreateNewCycle (data: neyCycleFormData) {
-    console.log(data)
+   const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    }
+
+    setCycles(state => [...state, newCycle])
+    setActiveCycle(id)
+
     reset()
   }
-
+  
   const task = watch('task')
   const isSubmitDisabled = !task
+  
+  const nowActiveCycle = cycles.find(cycle => cycle.id === activeCycle)
+
+  // Start count seconds and minutes for timer
+  const totalSeconds = nowActiveCycle ? nowActiveCycle.minutesAmount * 60 : 0
+  const currentSeconds = nowActiveCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount =  Math.floor(currentSeconds/ 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+  // End count seconds and minutes for timer
+
+  useEffect(() => {
+    if (nowActiveCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(differenceInSeconds(new Date(), nowActiveCycle.startDate))
+      }, 1000)
+    }
+  }, [nowActiveCycle])
+
 
   return (
     <HomeContainer>
@@ -56,11 +102,11 @@ export function Home() {
         </FormContainer>
 
         <CountdowmContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdowmContainer>
 
         <StartCountdownButton disabled={isSubmitDisabled} type="submit">
